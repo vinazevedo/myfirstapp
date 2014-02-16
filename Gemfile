cd530d4c333cd53d6de72f1231d63ac98a7e605b
Vinicius Azevedo <azevedo.vinicius@gmail.com>
2014-02-16 18:29:54 -0300

    Just set up Devise gem

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass', github: 'twbs/bootstrap-sass', branch: 'master'
 
+gem 'devise', '~> 3.2.2'
+
 group :development, :test do
 	gem 'sqlite3'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Myfirstapp::Application.configure do
   config.assets.initialize_on_precompile = true
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku.
+  # Note: set this to your actual host.
+  config.action_mailer.default_url_options = { :host => 'vsa-myfirstapp.herokuapp.com' }
 end
